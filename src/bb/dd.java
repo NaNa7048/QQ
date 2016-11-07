51f8f3e162dd4405b8a6abe652d5de0ea44f834d
Administrator <3265002880@qq.com>
2016-11-07 22:06:45 +0800

    首次提交到本地库

---
+++ src/bb/dd.java
@@ -0,0 +1,17 @@
+package bb;
+
+public class dd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			EagerSingleton s = EagerSingleton.getInstance();
+			EagerSingleton ss = EagerSingleton.getInstance();
+			if(s==ss){
+				System.out.println("œ‡Õ¨£°");
+			}
+		}
+
+
+	}
+
